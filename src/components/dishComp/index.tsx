89f1126ab9bbbc61e31c7cbfc0693234ddb7986c
SriramKannan04 <sriramsuka@gmail.com>
2026-03-14 01:56:25 +0530

    magor changes in grocery page

--- src/components/dishComp/index.tsx
+++ src/components/dishComp/index.tsx
@@ -12,7 +12,7 @@ import SplitText from "../../utilis/SplitText";
 const DishComponent = () => {
   return (
     <div className='w-full h-full flex flex-col gap-5'>
-      <div className='hidden lg:flex w-full h-1/5'></div>
+      <div className='hidden lg:flex w-full h-1/20'></div>
 
       <div className='w-full flex flex-col gap-3 p-3' style={{ border: "2px solid purple" }}>
         {/* <h1 className='text-3xl font-extrabold lg:text-4xl appTitle'>Food Menu</h1> */}
@@ -38,9 +38,6 @@ const DishComponent = () => {
 
       <div className='w-full h-full' style={{ border: "2px solid purple" }}>
 
-      </div>
-
-      <div className='w-full h-full' style={{ border: "2px solid purple" }}>
       </div>
     </div>
   )
