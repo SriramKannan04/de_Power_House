c0f9d4fc9f749c82cf223533109d75aa5f310cd0
SriramKannan04 <sriramsuka@gmail.com>
2026-09-03 20:36:12 +0530

    sept 3, 2026 - 1

--- src/components/groceriesComp/index.tsx
+++ src/components/groceriesComp/index.tsx
@@ -22,16 +22,18 @@ const GroceriesComponent = () => {
     setExplode(prev => (prev === exp ? null : exp));
   };
 
+
+  
   const groceriesList = [
     {
       id:1,
       category:"Cooking Essentials",
       items:[
-        {english:"Rice", tamil:"அரிசி", quantity:"5kg", img:imagePath.riceImg},
-        {english:"Pongal Rice", tamil:"பொங்கல் அரிசி", quantity:"1 Pack", img:imagePath.pongalRiceImg},
-        {english:"India Gate Briyani rice", tamil:"பிரியாணி அரிசி", quantity:"1kg", img:imagePath.briyaniRiceImg},
-        {english:"Sakthi Turmeric Powder", tamil:"மஞ்சள் தூள்", quantity:"250g", img:imagePath.turmericPowderImg},
-        {english:"Tata Salt", tamil:"கல் உப்பு", quantity:"500g", img:imagePath.saltImg},        
+        {english:"Rice",                    tamil:"அரிசி",              quantity:"5kg",       img:imagePath.riceImg},
+        {english:"Pongal Rice",             tamil:"பொங்கல் அரிசி",   quantity:"1 Pack",    img:imagePath.pongalRiceImg},
+        {english:"India Gate Briyani rice", tamil:"பிரியாணி அரிசி",   quantity:"1kg",       img:imagePath.briyaniRiceImg},
+        {english:"Sakthi Turmeric Powder",  tamil:"மஞ்சள் தூள்",       quantity:"250g",      img:imagePath.turmericPowderImg},
+        {english:"Tata Salt",               tamil:"கல் உப்பு",          quantity:"500g",      img:imagePath.saltImg},        
         // {english:"", tamil:"", quantity:"", img:""},
         // {english:"", tamil:"", quantity:"", img:""},
       ]
@@ -53,6 +55,29 @@ const GroceriesComponent = () => {
       ]
     },
   ];
+
+
+
+  //handle selection
+  const [selectedItems, setSelectedItems] = useState<number[]>([]);
+  const allSelect = groceriesList.flatMap(list => list.items).length > 0 && selectedItems.length === groceriesList.flatMap(list => list.items).length;
+  const someSelected =  selectedItems.length > 0 && selectedItems.length < groceriesList.flatMap(list => list.items).length;
+
+  const handleSelectAll = (event : React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      setSelectedItems(groceriesList.flatMap(list => list.items).map((_, index) => index));
+    } else {
+      setSelectedItems([]);
+    }
+  };
+
+  const handleSingleSelect = (index:number) => {
+    setSelectedItems((prev) =>
+      prev.includes(index)
+        ? prev.filter((item) => item !== index)
+        : [...prev, index]
+    );
+  };
 
   return (
     <div className='w-full h-full flex flex-col gap-5'>
@@ -81,11 +106,11 @@ const GroceriesComponent = () => {
 
       <div className='w-full p-3 flex flex-col justify-start items-center gap-3'>
         {groceriesList.map((list, index) => (
-          <Card key={index} className='w-full lg:w-3/4 p-3 arrowHandler' onClick={()=>expandHandler(Number(list.id))}>
+          <Card key={index} className='w-full lg:w-3/4 p-3'>
             <aside className='w-full flex flex-row justify-between items-center p-1'>
               <h1 className='text-sm lg:text-xl font-bold'>{list.category}</h1>
 
-              <aside>
+              <aside className="arrowHandler" onClick={()=>expandHandler(Number(list.id))}>
                 {explode === list.id ? <KeyboardArrowUpIcon /> :<KeyboardArrowDownIcon />}
               </aside>
             </aside>
@@ -95,18 +120,45 @@ const GroceriesComponent = () => {
                 <TableContainer  component={Paper} elevation={0} className="subTableBox">
                   <Table>
                     <TableHead className='headerRow'> 
-                      <TableRow>
-                        <TableCell className='subheaderCell'><Checkbox /></TableCell>
+                      <TableRow>                        
+                        <TableCell className='subheaderCell'>
+                          <Checkbox 
+                            className="selectAll" 
+                            checked={allSelect}
+                            indeterminate={someSelected}
+                            onChange={handleSelectAll}
+                            sx={{
+                            color: "#F2F2F2",
+                            '&.Mui-checked': {
+                              color: "#FFEDB9"
+                            },
+                          }}
+                          />
+                        </TableCell>
+
                         <TableCell className='subheaderCell'></TableCell>
                         <TableCell className='subheaderCell'>English</TableCell>
                         <TableCell className='subheaderCell'>Tamil</TableCell>
                         <TableCell className='subheaderCell'>Quantity</TableCell>
                       </TableRow>
                     </TableHead>
+
                     <TableBody>
                       {list.items.map((subData, index) => (
                         <TableRow key={index}>
-                          <TableCell className='subCell'><Checkbox /></TableCell>
+                          <TableCell className='subCell'>
+                            <Checkbox
+                              className="selectSingle"
+                              checked={selectedItems.includes(index)}
+                              onChange={() => handleSingleSelect(index)}
+                              sx={{
+                                color: "#000000",
+                                '&.Mui-checked': {
+                                  color: "#2C2C2C"
+                                },
+                              }} />
+                          </TableCell>
+
                           <TableCell className='subCell'><img src={subData.img}  width={80} style={{borderRadius:10}} /></TableCell>
                           <TableCell className='subCell'>{subData.english}</TableCell>
                           <TableCell className='subCell'>{subData.tamil}</TableCell>
@@ -115,8 +167,7 @@ const GroceriesComponent = () => {
                       ))}
                     </TableBody>
                   </Table>
-                </TableContainer>
-                
+                </TableContainer>                
               </Box>              
             )}            
           </Card>
--- src/components/homepageComp/index.tsx
+++ src/components/homepageComp/index.tsx
@@ -36,16 +36,19 @@ const HomePageComponent = () => {
             <img src={Homies} alt="Homies" width={500}/>
           </Box>
 
-          <Box className='' sx={{ display: { xl: "none", lg: "none", md: "none", sm: "block", xs: "block" } }}>
-            <img src={Homies} alt="Homies" width={300} />
+          <Box className='' sx={{ 
+            display: { xl: "none", lg: "none", md: "none", sm: "block", xs: "block" },
+            marginTop: { sm: "20px", xs: "20px" }
+            }}>
+            <img src={Homies} alt="Homies" width={300}/>
           </Box>
         </aside>
 
-        <aside className="w-full h-full flex gap-3 justify-center lg:justify-start items-center lg:items-start flex-col">
+        <aside className="w-full h-full flex gap-3 justify-center items-center flex-col lg:items-start">
           {/* <h1 className='text-3xl font-extrabold lg:text-5xl xl:text-5xl appTitle'>de Power House</h1> */}
           <SplitText
             text="de Power House"
-            className="text-3xl font-extrabold lg:text-5xl xl:text-5xl "
+            className="text-3xl font-extrabold lg:text-5xl xl:text-5xl"
             delay={50}
             duration={1.25}
             ease="power3.out"
@@ -58,17 +61,17 @@ const HomePageComponent = () => {
             // onLetterAnimationComplete={handleAnimationComplete}            
           />
 
-          <section className='p-3 flex flex-col justify-center items-left gap-3'>
+          <section className='p-3 w-5/6 flex flex-col justify-center items-start gap-3'> {/*  style={{border:"1px solid black"}}  */}
             <p className='text-wrap text-left italic font-semibold text-md'>
-              We roll as five, but we move as one. Three Chartered Accountants who balance numbers with precision,<br />
+              We roll as five, but we move as one. Three Chartered Accountants who balance numbers with precision,
               one Cost Management Accountant who finds value in every detail, and one techie who codes solutions into reality.
             </p>
             <p className='text-wrap text-left italic font-semibold text-md'>
-              As working professionals, we hustle through deadlines, audits, and projects, <br />
+              As working professionals, we hustle through deadlines, audits, and projects, 
               yet never lose the laughter, the tea breaks, or the late‑night talks that keep us grounded.
             </p>
             <p className='text-wrap text-left italic font-semibold text-md '>
-              Together we are more than colleagues; we are homies, a squad of growth, hustle, and friendship.<br />
+              Together we are more than colleagues; we are homies, a squad of growth, hustle, and friendship.
               Five professionals, one promise: homies for life.
             </p>
           </section>
@@ -122,20 +125,7 @@ const HomePageComponent = () => {
           </iframe>
 
           <p className='text-wrap text-start italic font-semibold text-sm'>10/7 Ruk Nivas, Raja Street, Devaraj Nagar, Saligramam, Chennai, TN 600093</p>
-        </aside> 
-
-          {/* <aside className="w-1/3 h-full flex flex-row gap-3 justify-start items-center">
-            <aside className='h-full mt-3'>
-              <img src={Location} alt="Location" width={25} />
-            </aside>
-
-            <aside className='w-full h-full'>
-              {address.map((item, index) => (
-                <p key={index} className='italic font-semibold text-xl '> {item.line} </p>
-              ))}
-            </aside>
-
-          </aside>             */}
+        </aside>           
       </Box>
 
       <Box className="w-full p-3 gap-3 justify-center lg:justify-start items-center lg:items-start flex-col lg:flex-row" sx={{display:{xl:"none", lg:"none", md:"flex", sm:"flex", xs:"flex"}}}>       
@@ -154,8 +144,8 @@ const HomePageComponent = () => {
             loading="lazy"
             allowFullScreen
             referrerPolicy="no-referrer-when-downgrade"
-            //src="https://maps.google.com/maps?q=De%20Power%20House%20Chennai&z=17&output=embed"
-            // src={`https://maps.google.com/maps?q=13.060979176925432,80.20280032225126&hl=es;z=14&output=embed`}
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d608.6854287330809!2d80.2028027714419!3d13.060916979935715!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a526700618a0f79%3A0x51396ae78d757fc!2sDe%20Power%20House!5e1!3m2!1sen!2sin!4v1788355226689!5m2!1sen!2sin"
+            //src={`https://maps.google.com/maps?q=13.060979176925432,80.20280032225126&hl=es;z=14&output=embed`}
           ></iframe>        
           
           <p className='text-wrap text-center italic font-semibold text-sm'>10/7 Ruk Nivas, Raja Street, Devaraj Nagar, Saligramam, Chennai, TN 600093</p>
@@ -169,8 +159,9 @@ const HomePageComponent = () => {
         <p className='text-wrap text-end italic font-semibold text-sm'>kindly ping me don't bite my <span className='text-lg'>d!ck</span></p>
       </Box>
 
-      <Box className='w-full p-5 justify-center items-center' sx={{display:{xl:"none", lg:"none", md:"block", sm:"block", xs:"block"}}}>
-        <p className='text-wrap text-center italic font-semibold text-sm'>My dear honourable 🐛🔥's if you have any suggestion,kindly ping me don't bite my d!ck</p>
+      <Box className='bg-gradient-to-r from-slate-900 to-slate-900 w-full p-5 justify-center items-center' sx={{display:{xl:"none", lg:"none", md:"block", sm:"block", xs:"block"}}}>
+        <p className='text-wrap text-center italic text-sm text-white'>My dear honourable 🐛🔥's if you have any suggestion</p>
+        <p className='text-wrap text-center italic text-sm text-white'>kindly ping me don't bite my d!ck</p>
       </Box>
     </div>
   )
